b131921aa7a01e4b506f1b9ac7d92fd9167bbb23
takahashisan <appler21op.mwp@docomo.ne.jp>
2016-07-07 12:10:05 +0000

    favorite機能のエラー修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,6 +3,6 @@ class Favorite < ActiveRecord::Base
 	belongs_to :event
 
 	validates :user, presence: true
-	validates :user_id, uniqueness: { scope: :post_id }
-	validates :post, presence: true
+	validates :event_id, uniqueness: { scope: :event_id }
+	validates :event, presence: true
 end
