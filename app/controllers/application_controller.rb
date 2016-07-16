7d2a795c5f634bbc6a0da3f48128af21b4cc172d
natsumi <natsumitsuna@gmail.com>
2016-07-16 09:29:33 +0000

    微妙な変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
 	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
   end
+
+  def configure_permitted_parameters
+  	 devise_parameter_sanitizer.for(:sign_up) << :email
+  end
 end
